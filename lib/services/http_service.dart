c0140e191f88aa09ee384f6151f15e5e383e3e91
YafetStefano <yafetstefano.ys10@gmail.com>
2021-04-20 11:55:33 +0700

    jb9

---
+++ lib/services/http_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:http_film/models/movie.dart';
+
+class HttpService {
+  final String apiKey = 'eee6c4bddcfc3ad6c1ab64c445e68585';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+  Future<List> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+}
